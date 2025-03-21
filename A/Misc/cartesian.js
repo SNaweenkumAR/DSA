fb8b48442db9a9eed960d44bdb25fdf3b9548feb
SNaweenkumAR <naween1924@gmail.com>
2025-03-21 11:05:17 +0530

    workout again

--- A/Misc/cartesian.js
+++ A/Misc/cartesian.js
@@ -2,7 +2,7 @@ function cartesian(arr1,arr2){
     let result = [];
     for(let i=0;i<arr1.length;i++){
         for( let j=0 ; j < arr2.length ; j++){
-             result.push([arr1[i],arr2[j]])
+             result.push([arr1[i]+arr2[j]])
         }
     }
          
--- A/Sorting/Quick.js
+++ A/Sorting/Quick.js
@@ -12,3 +12,21 @@ return [...Quick(left) , pivot,...Quick(right)];
 
 const arr = [3,9,1,8,-1,0,5]
 console.log(Quick(arr)); 
+
+
+
+
+// function dup(a){
+//   if(a.length < 2) return a;
+//   let pivot = a[a.length-1];
+//   let left=[];
+//   let right=[];
+//  for(i=0;i<a.length-1;i++){
+//   if(a[i]<pivot)left.push(a[i]);
+//   if(a[i]>pivot)right.push(a[i]);
+//  }
+// return [...dup(left),pivot,...dup(right)];
+// }
+
+// const ar = [3,9,1,8,-1,0,5]
+// console.log(dup(ar)); 
